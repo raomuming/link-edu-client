fbec8d4a1f1292efbf6d1493d3a92ebe6947e793
eric.rao <eric.rao@ringcentral.com>
2018-04-12 23:54:02 +0800

    request data after the page has been loaded.

--- xiancheng/pages/home/home.js
+++ xiancheng/pages/home/home.js
@@ -1,24 +1,24 @@
-// now, this list is for testing only
+
 var list;
 Page({
-  data:{},
-  onLoad:function(options){
-    this.setData({
-      list: list
-    })
-  }
-});
-
-// refer to: https://blog.csdn.net/YanzYan/article/details/54345828
-wx.request({
-  url: "https://www.link-edu.net/courses",
-  data: {},
-  method: "GET",
-  header: {
-    'content-type': 'application/json'
+  data:{
+    list: []
   },
-  success: function(res) {
-    console.log(res.data);
-    list = res.data;
+  onLoad:function(options){
+    let self = this;
+    // refer to: https://blog.csdn.net/YanzYan/article/details/54345828
+    wx.request({
+      url: "https://www.link-edu.net/courses",
+      data: {},
+      method: "GET",
+      header: {
+        "content-type": "application/json"
+      },
+      success: function(res) {
+        self.setData({
+          list: res.data
+        });
+      }
+    });
   }
 });
