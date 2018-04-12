19f1695f4f20df874c59e92d15e144119453ff01
eric.rao <eric.rao@ringcentral.com>
2018-04-12 23:35:27 +0800

    add course page, for a certain course.

--- xiancheng/pages/home/home.js
+++ xiancheng/pages/home/home.js
@@ -1,24 +1,5 @@
 // now, this list is for testing only
-var list = [{
-  id: 1,
-  name: 'Test for list',
-  avatar: 'https://wx1.sinaimg.cn/crop.0.32.827.465/90eb2137ly1fq8jp6r8pcj20mz0dwq4q.jpg'
-},
-{
-  id: 2,
-  name: 'Test for list 2',
-  avatar: 'https://wx1.sinaimg.cn/crop.0.32.827.465/90eb2137ly1fq8jp6r8pcj20mz0dwq4q.jpg'
-},
-{
-  id: 3,
-  name: 'Test for list 3',
-  avatar: 'https://wx1.sinaimg.cn/crop.0.32.827.465/90eb2137ly1fq8jp6r8pcj20mz0dwq4q.jpg'
-},
-{
-  id: 4,
-  name: 'Test for list 4',
-  avatar: 'https://wx1.sinaimg.cn/crop.0.32.827.465/90eb2137ly1fq8jp6r8pcj20mz0dwq4q.jpg'
-}];
+var list;
 Page({
   data:{},
   onLoad:function(options){
@@ -29,3 +10,15 @@ Page({
 });
 
 // refer to: https://blog.csdn.net/YanzYan/article/details/54345828
+wx.request({
+  url: "https://www.link-edu.net/courses",
+  data: {},
+  method: "GET",
+  header: {
+    'content-type': 'application/json'
+  },
+  success: function(res) {
+    console.log(res.data);
+    list = res.data;
+  }
+});
