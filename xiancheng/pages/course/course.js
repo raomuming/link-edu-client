b268418008ff7284fe2227b81de94995fc41a1e1
eric.rao <eric.rao@ringcentral.com>
2018-04-14 18:06:27 +0800

    go to course page, course id as parameter.

---
+++ xiancheng/pages/course/course.js
@@ -0,0 +1,35 @@
+Page({
+  data: {
+    course: null
+  },
+  onLoad: function(options) {
+    let self = this;
+    if (options.id != null && options.id > 0) {
+      self.fetchCourseById(options.id, (course) => {
+        if (course != null) {
+          self.setData({
+            course: course
+          });
+        }
+      });
+    }
+  },
+  fetchCourseById(id, callback) {
+    console.log('fetch course by id: ' + id);
+    wx.request({
+      url: "https://www.link-edu.net/course/" + id,
+      data: {},
+      method: "GET",
+      header: {
+        "content-type": "application/json"
+      },
+      success: function(res){
+        console.log("fetch course by id, result => ", res.data);
+        callback(res.data);
+      },
+      fail: function() {
+
+      }
+    });
+  }
+});
