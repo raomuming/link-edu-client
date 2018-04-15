6f428300f31740e7b79ae440e1540bd381f81d71
eric.rao <eric.rao@ringcentral.com>
2018-04-15 11:12:40 +0800

    implement login API.

--- xiancheng/app.js
+++ xiancheng/app.js
@@ -1,39 +1,57 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    let self = this;
     // 获取用户信息
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+          self.login((userInfo)=>{
+            if (userInfo) {
+              self.globalData.userInfo = userInfo;
             }
-          })
+          });
         }
       }
     })
   },
   globalData: {
     userInfo: null
+  },
+  login: function(callback) {
+    wx.login({
+      success: function(res) {
+        var code = res.code;
+        console.log("login code => ", code);
+        if (res.code) {
+          wx.getUserInfo({
+            fail: function(res){
+              // checkSettingStatus()
+            },
+            success: function(data) {
+              var encryptedData = data.encryptedData;
+              var iv = data.iv;
+              wx.request({
+                url: "https://www.link-edu.net/login",
+                data: {
+                  "code": code,
+                  "crypted_data": encryptedData,
+                  "iv": iv
+                },
+                method: "POST",
+                header: {
+                  "content-type": "application/json"
+                },
+                success: function(res) {
+                  console.log("login data =>", res.data);
+                  callback(res.data);
+                }
+              });
+            }
+          });
+        }
+      }
+    });
   }
 })
